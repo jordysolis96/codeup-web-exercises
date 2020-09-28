85163ea5e808e9de64c65cf4385fd192b9249e17
jordy muniz <jordy.muniz96@gmail.com>
2020-09-28 09:35:49 -0500

    added warm upfile

---
+++ js/warmup.js
@@ -0,0 +1,15 @@
+"use strict";
+
+console.log("hello hector");
+console.log("mac");
+console.log("henry");
+console.log("mary");
+console.log("lorian");
+
+function sayHello(name){
+    console.log("Hello " + name);
+//    we will talk about returning later!
+}
+
+sayHello("Vivian");
+sayHello("Sophie");
---
+++ word problems.js
@@ -0,0 +1,51 @@
+Word problems
+//exercise movie
+var littleMermaid = 3;
+var BrotherBear = 5;
+var Hurcules = 1;
+var totalPrice = (littleMermain + BrotherBear + Hurcules) * pricePerDay;
+
+var Google = 400 * 6;
+var Facebook = 350 * 10;
+var Amazon = 380 * 4;
+var week = Google + Facebook + Amazon
+
+
+//exercise class
+var ClassMax = x
+var CurrentNumberOfStudents = y 
+var claaOpen = (y<x)
+var noConflicts = true
+var enrolled = true
+if (classOpen && noConflicts){
+	enrolled
+}
+
+//exercise offer
+var numberOfItems = x;
+var customer = y;
+var offer = true;
+var status = true ;
+
+if (numberOfItems > 2 && offer = true) {
+	console.log('Enough products have been purchased for the product offer.')
+	return true;
+}
+if (status = true){
+	console.log('non-premium, not enough product purchase.')
+	return false
+}
+
+//exercise 4
+var username = 'codeup';
+var password = 'notastrongpassword';
+
+var chech = password.length>=5;
+var check = password !== username;
+var username.length<=20;
+
+var noSpaceUsername = username.trim();
+var noSpacepassword = password.trim();
+var checkSpacesUsername = username === noSpaceUsername
+var checkSpacesPassword = password === noSpacepassword
+
